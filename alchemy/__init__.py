a7299427d50550fe116095d49f2d8dd44098de2d
peladno <j.perezurrutia@gmail.com>
2026-07-22 20:18:23 +0900

    error importing riectly fixed

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -2,6 +2,7 @@
 from .elements import create_air
 from .potions import strength_potion
 from .potions import healing_potion as heal
+from . import transmutation
 
 
-__all__ = ["create_air", "heal", "strength_potion"]
+__all__ = ["create_air", "heal", "strength_potion", "transmutation"]
